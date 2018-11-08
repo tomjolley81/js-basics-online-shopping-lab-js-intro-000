f44e829e4dd94f40b40a88a03ae76c7bd504c35a
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 18:16:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ function viewCart() {
   }else if (cart.length > 1 && cart.length < 3) {
     return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
   }else {
+    var itemNames = []
+    var itemPrices = []
+    itemNames.push(cart.map(({itemName})=> itemName))
+    itemPrices.push(cart.map(({itemPrice})=> itemPrice))
     for (var i = 0; i < cart.length; i++) {
       var string = []
-      var itemNames = []
-      var itemPrices = []
-      itemNames.push(cart.map(({itemName})=> itemName))
-      itemPrices.push(cart.map(({itemPrice})=> itemPrice))
       string.push(`${itemNames[i]} at ${itemPrices[i]}`)
     }
 
