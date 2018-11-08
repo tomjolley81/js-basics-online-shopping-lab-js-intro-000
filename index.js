75017781daea765cb847b8ef6d11d26b987dc01d
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 17:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,16 +25,20 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  var nm = 0
+  var i = 0
+  function incrementVariable() {
+    i = i + 1
+  }
   if(!cart.length) {
     return "Your shopping cart is empty."
   }
   if(cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }else if (cart.length === 2) {
-    for (var i = 0; i <= cart.length; i++) {
+    while (i < cart.lenght) {
       var strings = []
       strings.push(`${getCart()[i].itemName} at $${getCart()[i].itemPrice}`)
+      incrementVariable()
     }
     console.log(strings)
     return `In your cart, you have ${strings[0]}, and ${strings[1]}.`
