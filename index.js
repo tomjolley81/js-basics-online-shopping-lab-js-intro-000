1835028ec4582b379445226e59e61f905c159768
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 18:33:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,12 +32,17 @@ function viewCart() {
 
   if(!cart.length) {
     return "Your shopping cart is empty."
+  }else {
+    if(cart.length === 1) {
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    }else if (cart.length > 1 && cart.length < 3) {
+      return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
+    }
+    for (var i = 0; i < cart.length; i++) {
+      return cart
+    }
   }
-  if(cart.length === 1) {
-    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
-  }else if (cart.length > 1 && cart.length < 3) {
-    return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
-  }else {}
+  
 
 }
 
