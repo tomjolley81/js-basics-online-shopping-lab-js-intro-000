67476c97e55a03e28f9f1db4752b71d49b740e86
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 17:08:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function viewCart() {
   }else if (cart.length === 2) {
     for (var i = 0; i < cart.length; i++) {
       var strings = []
-      strings.push(`${getCart()[i].itemName} at ${getCart()[i].itemPrice}`)
+      strings.push(`${getCart()[i-1].itemName} at $${getCart()[i-1].itemPrice}`)
     }
     return `In your cart, you have ${strings[0]}, and ${strings[1]}`
   }
