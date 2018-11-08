babbbd88518f7c46a8829c93683d2fd3c4864444
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 18:30:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }else if (cart.length > 1 && cart.length < 3) {
     return `In your cart, you have ${getCart()[0].itemName} at $${getCart()[0].itemPrice}, and ${getCart()[1].itemName} at $${getCart()[1].itemPrice}.`
-  }else
+  }else {}
 
 }
 
