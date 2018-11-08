ca2d5835dedc56865477e16f4027caf2c31af731
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 16:46:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ price = 0
  var itemToAdd = {}
  itemToAdd = {
    itemName : item
+   itemPrice: price
  }
+ 
+ function getPrice() {
+   var price =  Math.floor(Math.random() * Math.floor(100))
+   return price
+ }
+ getPrice()
  cart.push(itemToAdd)
  return cart
 }
