220090e704dc1191712503555a57b36931159564
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 17:20:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function viewCart() {
   if(cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   }else if (cart.length > 1 && cart.length < 3) {
-    while (i < cart.length) {
+    while (i <= cart.length) {
       var strings = []
       strings = [...strings, `${getCart()[i].itemName} at $${getCart()[i].itemPrice}`]
       incrementVariable()
